e16ef761a65ee4a521bb3c2b70fe0e31bea120c0
Mattias Fält <faldt.mattias@gmail.com>
2021-12-02 09:31:39 +0100

    better day1

--- day1/code.jl
+++ day1/code.jl
@@ -11,6 +11,16 @@ function day1_1(data)
 end
 
 function day1_2(data)
+    prevs = (typemax(Int),typemax(Int),typemax(Int))
+    s = 0
+    for dat in data
+        s += dat>prevs[1]
+        prevs = (prevs[2], prevs[3], dat)
+    end
+    s
+end
+
+function day1_2_naive(data)
     prev = typemax(Int)
     s = 0
     @inbounds for i in 1:(length(data)-2)
@@ -21,14 +31,13 @@ function day1_2(data)
     s
 end
 
-day1_1_min(data) =
-    mapfoldl(v->(v,0), # Keep sum in second element
-        (prev,now) -> (now[1], prev[2] + Int(now[1]>prev[1])), # Save old in first element
+day1_1_min(data) = foldl(
+        (prev,now) -> (now, prev[2] + Int(now>prev[1])), # Save old in first element
         data, init=(typemax(Int),0))[2] # typemax to not count first, start sum at 0
 
-day1_2_min(data) =
-    @inbounds day1_1_min(data[i]+data[i+1]+data[i+2] for i in 1:(length(data)-2))
-
+day1_2_min(data) = foldl(
+        (prev,now) -> (prev[2], prev[3], now, prev[4] + Int(now>prev[1])), # Save old in 3 first elements
+        data, init=(typemax(Int),typemax(Int),typemax(Int),0))[4] # typemax to not count first, start sum at 0
 
 data = readfile("day1/input.txt", Int)
 
